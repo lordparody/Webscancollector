043964112f2d1c7c40f1fca7ebd02fdbaa211aa6
pARODY <parody@lordparody.com>
2019-01-11 15:01:23 +1000

    Update index.php
    Cleaned up output for Splunk ingestion.
    
    Changed output to JSON style values

--- webscancollector/index.php
+++ webscancollector/index.php
@@ -1,52 +1,31 @@
 <?php
 // https://gist.github.com/magnetikonline/650e30e485c0f91f2f40
-// nginx redirection  ++  location / { try_files $uri $uri/ /index.php$is_args$args; }  ++ 
+// nginx redirection  ++  location / { try_files $uri $uri/ /index.php$is_args$args; }  ++
 // works on IIS with PHP installed
+// version 0.2 - code assistance from S7oRm
 
 class DumpHTTPRequestToFile {
 
-	public function execute($targetFile) {
+        public function execute($targetFile) {
+                $datetime = microtime(true);
 
-		$data = sprintf(
-			"%s %s %s %s\n\nHTTP headers:\n",
-			$_SERVER['REQUEST_METHOD'],
-			$_SERVER['REQUEST_URI'],
-			$_SERVER['REMOTE_ADDR'],
-			$_SERVER['SERVER_PROTOCOL']
-		);
+                $mydata->time = $datetime;
+                $mydata->Headers = $_SERVER;
+                $mydata->Request = $_REQUEST;
 
-		foreach ($this->getHeaderList() as $name => $value) {
-			$data .= $name . ': ' . $value . "\n";
-		}
 
-		$data .= "\nRequest body:\n";
+                $myJSON = json_encode($mydata,JSON_UNESCAPED_SLASHES);
 
-		file_put_contents(
-			$targetFile,
-			$data . print_r($_REQUEST,true) . "\n*************************************************************\n", FILE_APPEND
-		);
 
-		echo("Page not found\n\n");
-	}
+                file_put_contents(
+                        $targetFile,
+                        $myJSON . "\n", FILE_APPEND
+                );
 
-	private function getHeaderList() {
-
-		$headerList = [];
-		foreach ($_SERVER as $name => $value) {
-			if (preg_match('/^HTTP_/',$name)) {
-				// convert HTTP_HEADER_NAME to Header-Name
-				$name = strtr(substr($name,5),'_',' ');
-				$name = ucwords(strtolower($name));
-				$name = strtr($name,' ','-');
-
-				// add to list
-				$headerList[$name] = $value;
-			}
-		}
-
-		return $headerList;
-	}
+                echo("Page not found\n\n");
+        }
 }
 
 
 (new DumpHTTPRequestToFile)->execute('./dumprequest.txt');
+?>
